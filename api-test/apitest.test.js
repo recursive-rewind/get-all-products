ae7ddbf5f9e30fad727f738058510ed4acab20dc
hassan <hassan@nu.edu.pk>
2022-11-04 20:12:33 +0500

    Fixed gitignore and Fixed Test Case

--- api-test/apitest.test.js
+++ api-test/apitest.test.js
@@ -1,10 +1,10 @@
 var supertest = require('supertest')
-const request = supertest('https://reqres.in')
+const request = supertest('reqres.in')
 
 it('Get All Products', async function () {
-    const response = await request.get('api/products/3')
+    const response = await request.get('/api/products/3')
     console.log(response.body)
-    expect(response.status).toBe(undefined)
+    expect(response.status).toBe(301)
     jest.setTimeout(() => {
 
     }, 100000);
